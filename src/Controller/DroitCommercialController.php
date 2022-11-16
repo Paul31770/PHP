3f830af7035ad458b7430c93eb0dac7a0bd39db6
Samuel-lgd <lagarde.samuel@gmail.com>
2022-11-16 11:23:52 +0100

    creation controller + liens header

--- src/Controller/DroitCommercialController.php
+++ src/Controller/DroitCommercialController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DroitSociétésController extends AbstractController
+class DroitCommercialController extends AbstractController
 {
-    #[Route('/droitsocietes', name: 'app_droitsocietes')]
+    #[Route('/droitcommercial', name: 'app_droit_commercial')]
     public function index(): Response
     {
-        return $this->render('droit_sociétés/index.html.twig', [
-            'controller_name' => 'DroitSociétésController',
+        return $this->render('droit_commercial/index.html.twig', [
+            'controller_name' => 'DroitCommercialController',
         ]);
     }
 }
